25b838b46800e313c58c712b5cf8f57fc81eb15f
Andhitia Rama <andhitia.r@gmail.com>
2017-03-29 21:21:46 +0700

    [ADD][8.0] analytic_partner_hr_timesheet_report

---
+++ analytic_partner_hr_timesheet_report/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Partner information on Timesheet Analysis Report",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Human Resources",
+    "depends": [
+        "analytic_partner_hr_timesheet",
+    ],
+    "data": [
+        "reports/hr_timesheet_report_views.xml"
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
